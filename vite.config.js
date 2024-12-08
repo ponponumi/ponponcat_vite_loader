0fc2365179128014659005fc5d6b1005b83cc2cd
ponponumi <ponponumi@gmail.com>
2024-12-08 13:07:01 +0900

    Viteで読み込み

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         outDir: './test_theme/build',
         rollupOptions: {
             input: [
-                //
+                "assets/ts/script.ts"
             ],
         },
     },
