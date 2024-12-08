5391c41dc65d0d3894d126ffecc9fb62330ca488
ponponumi <ponponumi@gmail.com>
2024-12-08 13:14:34 +0900

    試しにコンストラクタを作成(成功)

--- test_theme/functions.php
+++ test_theme/functions.php
@@ -1,3 +1,8 @@
 <?php
 
 require_once __DIR__ . "/vendor/autoload.php";
+
+(function () {
+    $viteLoader = new \Ponponumi\PonponcatViteLoader\Load(__DIR__ . "/build/.vite/manifest.json",get_template_directory_uri() . "/build");
+    var_dump($viteLoader);
+})();
