873d800a8abdc6a089883fe3f845db30e3d62f42
ponponumi <ponponumi@gmail.com>
2024-12-09 00:13:18 +0900

    読み込ませるコードを追加

--- test_theme/functions.php
+++ test_theme/functions.php
@@ -4,5 +4,19 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 (function () {
     $viteLoader = new \Ponponumi\PonponcatViteLoader\Load(__DIR__ . "/build/.vite/manifest.json",get_template_directory_uri() . "/build");
+
+    $viteLoader->filesSet([
+        "assets/scss/style.scss",
+        "assets/ts/script.ts",
+    ]);
+
+    $viteLoader->filesSet([
+        "assets/ts/head.ts",
+    ],"head");
+
+    $viteLoader->filesSet([
+        "assets/ts/footer.ts",
+    ],"footer");
+
     var_dump($viteLoader);
 })();
