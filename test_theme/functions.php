862d2ccdcdb20cb91903feb95c56d42be53960da
ponponumi <ponponumi@gmail.com>
2024-12-09 01:10:17 +0900

    エラーモードを有効にし、存在しないファイルを渡すように変更

--- test_theme/functions.php
+++ test_theme/functions.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 (function () {
-    $viteLoader = new \Ponponumi\PonponcatViteLoader\Load(__DIR__ . "/build/.vite/manifest.json",get_template_directory_uri() . "/build");
+    $viteLoader = new \Ponponumi\PonponcatViteLoader\Load(__DIR__ . "/build/.vite/manifest.json",get_template_directory_uri() . "/build",true);
 
     if(WP_DEBUG){
         $viteLoader->devSet(true, $_ENV["PONPONCAT_VITE_HOST"], $_ENV["PONPONCAT_VITE_HOST_WEB"]);
@@ -20,6 +20,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 
     $viteLoader->filesSet([
         "assets/ts/footer.ts",
+        "assets/ts/fooe.ts",
     ],"footer");
 
     $viteLoader->load();
