400773362ee688572db43c97259ba60d01d18206
ponponumi <ponponumi@gmail.com>
2024-12-09 00:45:18 +0900

    エラーが起こらない事を確認

--- test/index.php
+++ test/index.php
@@ -4,19 +4,4 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 (function () {
     $viteLoader = new \Ponponumi\PonponcatViteLoader\Load(__DIR__ . "/build/.vite/manifest.json","/build");
-
-    $viteLoader->filesSet([
-        "assets/scss/style.scss",
-        "assets/ts/script.ts",
-    ]);
-
-    $viteLoader->filesSet([
-        "assets/ts/head.ts",
-    ],"head");
-
-    $viteLoader->filesSet([
-        "assets/ts/footer.ts",
-    ],"footer");
-
-    $viteLoader->load();
 })();
