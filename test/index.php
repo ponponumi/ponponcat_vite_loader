75c11b6945e95666a462d25993e70e28ca5f8dc9
ponponumi <ponponumi@gmail.com>
2024-12-09 00:44:59 +0900

    エラーが起こる事を確認

--- test/index.php
+++ test/index.php
@@ -1,3 +1,22 @@
 <?php
 
 require_once __DIR__ . "/vendor/autoload.php";
+
+(function () {
+    $viteLoader = new \Ponponumi\PonponcatViteLoader\Load(__DIR__ . "/build/.vite/manifest.json","/build");
+
+    $viteLoader->filesSet([
+        "assets/scss/style.scss",
+        "assets/ts/script.ts",
+    ]);
+
+    $viteLoader->filesSet([
+        "assets/ts/head.ts",
+    ],"head");
+
+    $viteLoader->filesSet([
+        "assets/ts/footer.ts",
+    ],"footer");
+
+    $viteLoader->load();
+})();
