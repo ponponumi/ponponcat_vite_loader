6b3eed1dd5d4c0dc6864d10856e8ea8719b65369
ponponumi <ponponumi@gmail.com>
2024-12-08 13:10:39 +0900

    プロパティを追加

--- src/Load.php
+++ src/Load.php
@@ -4,5 +4,10 @@ namespace Ponponumi\PonponcatViteLoader;
 
 class Load
 {
-    //
+    public array $cssFiles = [];
+    public array $jsModuleFiles = [];
+    public array $jsFiles = [];
+    public array $jsFooterFiles = [];
+    public object $viteLoader;
+    public $devMode = false;
 }
