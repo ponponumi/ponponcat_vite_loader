55e41e833b7c9e1b92ddf377e2c738ab4315f114
ponponumi <ponponumi@gmail.com>
2024-12-09 01:11:31 +0900

    エラーが消える事を確認

--- test_theme/functions.php
+++ test_theme/functions.php
@@ -6,7 +6,7 @@ require_once __DIR__ . "/vendor/autoload.php";
     $viteLoader = new \Ponponumi\PonponcatViteLoader\Load(__DIR__ . "/build/.vite/manifest.json",get_template_directory_uri() . "/build",true);
 
     if(WP_DEBUG){
-        // $viteLoader->devSet(true, $_ENV["PONPONCAT_VITE_HOST"], $_ENV["PONPONCAT_VITE_HOST_WEB"]);
+        $viteLoader->devSet(true, $_ENV["PONPONCAT_VITE_HOST"], $_ENV["PONPONCAT_VITE_HOST_WEB"]);
     }
 
     $viteLoader->filesSet([
