c6036e5629dd61372885c1286db64893b8762738
ponponumi <ponponumi@gmail.com>
2024-12-08 13:17:21 +0900

    デバッグモードを設定するメソッドを作成

--- src/Load.php
+++ src/Load.php
@@ -15,4 +15,17 @@ class Load
     {
         $this->viteLoader = new \Ponponumi\ViteLoader\ViteLoader($manifestPath, $buildPath, $errorMode);
     }
+
+    public function devSet($devMode=false,string $devHost="",string $devHostWeb=""){
+        // デバッグモードを設定する
+        if($devMode){
+            $this->viteLoader->devServerSetting($devMode, $devHost, $devHostWeb);
+
+            // リロード用スクリプトを取得し、失敗すればデバッグモードは無効にする
+            $reloadHTML = $this->viteLoader->viteReloadHtmlGet(false);
+            $this->devMode = $reloadHTML ? true : false;
+        }else{
+            $this->devMode = false;
+        }
+    }
 }
